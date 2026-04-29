eb69e0af2bae416a84558679aa05897bf165a80c
saivaraprasadmuvvala <saivara8500@gmail.com>
2026-04-29 06:22:58 +0000

    pyramidNumber.java

---
+++ pyramidNumber.java
@@ -0,0 +1,13 @@
+public class pyramidNumber {
+    public static void main(String[] args) {
+            for(int i = 0; i <=5; i++){
+                for(int k = 5-i; k >=0; k--){
+                    System.out.print(" ");
+                }
+                for(int j = 0; j<=i; j++){
+                        System.out.print(j+" ");
+                    }
+                System.out.println();
+            }
+    }
+}
