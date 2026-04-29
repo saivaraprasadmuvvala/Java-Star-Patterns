b807b2039e66e148b83d3227d9be0f136acb29ee
saivaraprasadmuvvala <saivara8500@gmail.com>
2026-04-29 10:23:42 +0000

    halfRhombus.java

---
+++ halfRhombus.java
@@ -0,0 +1,16 @@
+public class halfRhombus {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j<=i; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < 5; i++){
+            for(int j = 4-i; j>0; j--){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
