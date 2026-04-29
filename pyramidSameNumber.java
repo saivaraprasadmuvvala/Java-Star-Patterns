547bd7551b3a3dc3aa29937cee88cfb2462644f4
saivaraprasadmuvvala <saivara8500@gmail.com>
2026-04-29 06:25:13 +0000

    pyramidSameNumber.java

---
+++ pyramidSameNumber.java
@@ -0,0 +1,13 @@
+public class pyramidSameNumber {
+    public static void main(String[] args) {
+        for(int i = 0; i <=5; i++){
+            for(int k = 5-i; k >=0; k--){
+                System.out.print(" ");
+            }
+            for(int j = 0; j<=i; j++){
+                    System.out.print(i+" ");
+                }
+            System.out.println();
+        }
+    }
+}
