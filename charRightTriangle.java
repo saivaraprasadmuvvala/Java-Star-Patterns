acd3c95db4f6ca363c83a6f5c48affd2fe35f7d6
saivaraprasadmuvvala <saivara8500@gmail.com>
2026-04-29 11:59:30 +0000

    charRightTriangle.java

---
+++ charRightTriangle.java
@@ -0,0 +1,12 @@
+public class charRightTriangle {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++){
+            char sai = 'A';
+            for(int j = 1; j<=i; j++){
+                System.out.print(sai+" ");
+                sai++;
+            }
+            System.out.println();
+        }
+    }
+}
