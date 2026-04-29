a8ea9a2e0cfd9934620cc8d8bfb4907ead811ace
saivaraprasadmuvvala <saivara8500@gmail.com>
2026-04-29 10:08:34 +0000

    rhombusBigPyramid.java

---
+++ rhombusBigPyramid.java
@@ -0,0 +1,30 @@
+public class rhombusBigPyramid {
+    public static void main(String[] args){
+        for(int i=0; i<=5; i++){
+            for(int j=0; j<=5-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k <= 2*i+1; k++){
+                System.out.print("*");
+            }
+            for(int l=0; l<=5-i-1; l++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        
+        for(int i=0; i<=5; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<=(2*5)-(i*2)-1; k++){
+                System.out.print("*");
+            }
+            for(int l=0; l<=i; l++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
